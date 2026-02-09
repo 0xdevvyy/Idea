5ac9270a4899b3d8823b62436f6b74cedc918335
0xdevvyy <pullhead45@gmail.com>
2026-02-09 15:06:11 +0800

    feat(flash): added alpinejs to flash a session message

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>IDEA</title>
-    @vite(['resources/css/app.css'])
+     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-background text-foreground">
     <x-layout.nav />
@@ -13,6 +13,25 @@
         {{ $slot }}
     </main>
     
-    
+   @session('success')
+    <div
+        x-data="{ show: true }"
+         x-init="
+        setTimeout(() => {
+            show = false
+            console.log('closed')
+        }, 3000)
+    "
+        x-show="show"
+        x-transition.opacity.duration.300ms
+        x-cloak
+        class="bg-primary text-white px-4 absolute bottom-4 right-4 rounded-lg"
+    >
+        testing
+    </div>
+    @endsession
+
+        
+
 </body>
 </html>
