fd15d5e3b3985cef7809d5abb5051f020fc5dcbb
0xdevvyy <pullhead45@gmail.com>
2026-02-09 16:10:24 +0800

    feat(cards): added the styling for the cards

--- database/migrations/2026_02_07_090054_create_ideas_table.php
+++ database/migrations/2026_02_07_090054_create_ideas_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('ideas', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
-            $table->string('title');
+            $table->string('title')->nullable();
             $table->string('description')->nullable();
             $table->json('links')->nullable();
             $table->string('image_path')->nullable();
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,32 @@
 <?php
 
+use App\Http\Controllers\IdeasController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('welcome'));
+// Route::get('/', fn () => view('welcome'));
+Route::redirect('/', '/ideas');
+
+
+Route::middleware('auth')->group(function(){
+    Route::get('/ideas', [IdeasController::class, 'index'])->name('ideas.index');
+    Route::get('/idea/{ideas}', [IdeasController::class, 'show'])->name('ideas.show');
+
+    Route::post('logout', [SessionsController::class, 'destroy']);
+});
+
 
 Route::middleware('guest')->group(function(){
     
     Route::get('/register', [RegisteredUserController::class, 'create']);
     Route::post('/register', [RegisteredUserController::class, 'store']);
 
-    Route::get('/login', [SessionsController::class, 'create']);
+    Route::get('/login', [SessionsController::class, 'create'])->name('login');
     Route::post('/login', [SessionsController::class, 'store']);
 });
 
-Route::post('logout', [SessionsController::class, 'destroy']);
+
 
 
 
