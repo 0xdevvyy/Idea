f267df1a98084d9ea084f401db352384ada5fe5d
0xdevvyy <pullhead45@gmail.com>
2026-02-12 11:09:02 +0800

    feat(modal): created a functional modal using alpinejs

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,31 @@
+@props(['name', 'title'])
+
+<div 
+    x-data="{show: false, name: @js($name)}"
+    x-show="show"
+    @keydown.escape.window="show = false"
+    @open-modal.window="if($event.detail === name) show = true"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-xs"
+    style="display: none"
+    x-transition:enter="ease-out duration-200"
+    x-transition:enter-start="opacity-0 -translate-y-4 -translate-x-4"
+    x-transition:enter-end="opacity-100"
+    x-transition:leave="ease-in duration-150"
+    x-transition:leave-start="opacity-100"
+    x-transition:leave-end="opacity-0 -translate-y-4 -translate-x-4"
+    role="dialog"
+    aria-modal="true"
+    aria-labelledby="modal-{{ $name }}-title"
+    :aria-hidden="!show"
+    tabindex="-1"
+>
+    <x-card @click.away="show = false">
+        <div id="modal-{{ $name }}-title" class="font-bold text-xl">
+            {{$title}}
+        </div>
+        <div>
+            {{ $slot }}
+        </div>
+
+    </x-card>
+</div>
